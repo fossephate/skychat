05c5174d502fc78ffa1708ad6c1af6a55ff93d50
Matthew Fosse <matt@fosse.co>
2025-02-26 18:27:04 -0800

    save:

--- mobile/src/app/(app)/chats/[groupId].tsx
+++ mobile/src/app/(app)/chats/[groupId].tsx
@@ -1,6 +1,6 @@
 // import ChatMessageBox from '@/components/Chat/ChatMessageBox';
 // import ReplyMessageBar from '@/components/Chat/ReplyMessageBar';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons"
 // import React, { useState, useCallback, useEffect, useRef } from 'react';
 // import { ImageBackground, StyleSheet, View, ViewStyle } from 'react-native';
 // import { Swipeable } from 'react-native-ge, ViewStylesture-handler';
@@ -13,31 +13,37 @@ import { Ionicons } from '@expo/vector-icons';
 //   IMessage,
 // } from 'react-native-gifted-chat';
 // import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import messageData from 'assets/data/messages.json';
+import messageData from "assets/data/messages.json"
 // import { useAppTheme } from '@/utils/useAppTheme';
-import { ThemedStyle } from '@/theme';
+import { ThemedStyle } from "@/theme"
 
-import ChatMessageBox from "@/components/Chat/ChatMessageBox";
-import ReplyMessageBar from "@/components/Chat/ReplyMessageBar";
-import { useAppTheme } from "@/utils/useAppTheme";
-import React, { useState, useRef, useEffect, useCallback } from "react";
-import { ImageBackground, View, ViewStyle, Image } from "react-native";
-import { Swipeable } from "react-native-gesture-handler";
-import { IMessage, GiftedChat, SystemMessage, Bubble, Send, InputToolbar } from "react-native-gifted-chat";
-import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
-import { Header, Text } from '@/components';
-import { router } from 'expo-router';
+import ChatMessageBox from "@/components/Chat/ChatMessageBox"
+import ReplyMessageBar from "@/components/Chat/ReplyMessageBar"
+import { useAppTheme } from "@/utils/useAppTheme"
+import React, { useState, useRef, useEffect, useCallback } from "react"
+import { ImageBackground, View, ViewStyle, Image } from "react-native"
+import { Swipeable } from "react-native-gesture-handler"
+import {
+  IMessage,
+  GiftedChat,
+  SystemMessage,
+  Bubble,
+  Send,
+  InputToolbar,
+} from "react-native-gifted-chat"
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context"
+import { Header, Text } from "@/components"
+import { router } from "expo-router"
 
 const Page = () => {
-  const [messages, setMessages] = useState<IMessage[]>([]);
-  const [text, setText] = useState('');
-  const insets = useSafeAreaInsets();
+  const [messages, setMessages] = useState<IMessage[]>([])
+  const [text, setText] = useState("")
+  const insets = useSafeAreaInsets()
 
-  const [replyMessage, setReplyMessage] = useState<IMessage | null>(null);
-  const swipeableRowRef = useRef<Swipeable | null>(null);
+  const [replyMessage, setReplyMessage] = useState<IMessage | null>(null)
+  const swipeableRowRef = useRef<Swipeable | null>(null)
 
-  const { themed, theme } = useAppTheme();
-
+  const { themed, theme } = useAppTheme()
 
   useEffect(() => {
     setMessages([
@@ -48,26 +54,26 @@ const Page = () => {
           createdAt: new Date(message.date),
           user: {
             _id: message.from,
-            name: message.from ? 'You' : 'Bob',
+            name: message.from ? "You" : "Bob",
           },
-        };
+        }
       }),
       {
         _id: 0,
         system: true,
-        text: 'All your base are belong to us',
+        text: "All your base are belong to us",
         createdAt: new Date(),
         user: {
           _id: 0,
-          name: 'Bot',
+          name: "Bot",
         },
       },
-    ]);
-  }, []);
+    ])
+  }, [])
 
   const onSend = useCallback((messages = []) => {
-    setMessages((previousMessages: any[]) => GiftedChat.append(previousMessages, messages));
-  }, []);
+    setMessages((previousMessages: any[]) => GiftedChat.append(previousMessages, messages))
+  }, [])
 
   const renderInputToolbar = (props: any) => {
     return (
@@ -75,13 +81,13 @@ const Page = () => {
         {...props}
         containerStyle={{ backgroundColor: theme.colors.background }}
         renderActions={() => (
-          <View style={{ height: 44, justifyContent: 'center', alignItems: 'center', left: 5 }}>
+          <View style={{ height: 44, justifyContent: "center", alignItems: "center", left: 5 }}>
             <Ionicons name="add" color={theme.colors.palette.primary300} size={28} />
           </View>
         )}
       />
-    );
-  };
+    )
+  }
 
   const updateRowRef = useCallback(
     (ref: any) => {
@@ -90,124 +96,139 @@ const Page = () => {
         replyMessage &&
         ref.props.children.props.currentMessage?._id === replyMessage._id
       ) {
-        swipeableRowRef.current = ref;
+        swipeableRowRef.current = ref
       }
     },
-    [replyMessage]
-  );
+    [replyMessage],
+  )
 
   useEffect(() => {
     if (replyMessage && swipeableRowRef.current) {
-      swipeableRowRef.current.close();
-      swipeableRowRef.current = null;
+      swipeableRowRef.current.close()
+      swipeableRowRef.current = null
     }
-  }, [replyMessage]);
+  }, [replyMessage])
+
+
+  let backgroundImage;
+  if (theme.isDark) {
+    backgroundImage = "https://images.unsplash.com/photo-1534841090574-cba2d662b62e?q=80&w=3987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  } else {
+    backgroundImage = "https://images.unsplash.com/photo-1599435214324-d71096238079?q=80&w=3987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  }
 
   return (
-    // <ImageBackground
-    //   // source={require('@/assets/images/pattern.png')}
-    //   style={{
-    //     flex: 1,
-    //     backgroundColor: theme.colors.background,
-    //     marginBottom: insets.bottom,
-    //   }}>
-    <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.background }}>
+    <ImageBackground
+      source={{ uri: backgroundImage }}
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+        marginBottom: insets.bottom,
+      }}>
+      {/* <View style={{ flex: 1, backgroundColor: theme.colors.background }}> */}
         <Header title="Chat with Bob" leftIcon="back" onLeftPress={() => router.back()} />
-      <GiftedChat
-        messages={messages}
-        onSend={(messages: any) => onSend(messages)}
-        onInputTextChanged={setText}
-        user={{
-          _id: 1,
-        }}
-        renderSystemMessage={(props) => (
-          <SystemMessage {...props} textStyle={{ color: theme.colors.palette.neutral300 }} />
-        )}
-        bottomOffset={insets.bottom}
-        renderAvatar={() => {
-          // use pravatar:
-          const groupId = "123";
-          return <Image source={{ uri: `https://i.pravatar.cc/150?u=${groupId}` }} style={{ width: 32, height: 32 }} />
-        }}
-        maxComposerHeight={100}
-        textInputProps={themed($composer)}
-        renderBubble={(props) => {
-          return (
-            <Bubble
-              {...props}
-              textStyle={{
-                right: {
-                  color: '#000',
-                },
-              }}
-              wrapperStyle={{
-                left: {
-                  backgroundColor: '#fff',
-                },
-                right: {
-                  backgroundColor: theme.colors.palette.secondary300,
-                },
-              }}
-            />
-          );
-        }}
-        // renderAccessory={() => {
-        //   const groupId = "456";
-        //   return <Image source={{ uri: `https://i.pravatar.cc/150?u=${groupId}` }} style={{ width: 32, height: 32 }} />
-        // }}
-        isTyping={true}
-        infiniteScroll
-        onPressActionButton={() => {
-          console.log("action button pressed");
-        }}
-        isScrollToBottomEnabled={true}
-        renderSend={(props) => (
-          <View
-            style={{
-              height: 44,
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'center',
-              gap: 14,
-              paddingHorizontal: 14,
-            }}>
-            {text === '' && (
-              <>
-                <Ionicons name="camera-outline" color={theme.colors.palette.primary300} size={28} />
-                <Ionicons name="mic-outline" color={theme.colors.palette.primary300} size={28} />
-              </>
-            )}
-            {text !== '' && (
-              <Send
+        <GiftedChat
+          messages={messages}
+          onSend={(messages: any) => onSend(messages)}
+          onInputTextChanged={setText}
+          user={{
+            _id: 1,
+          }}
+          renderSystemMessage={(props) => (
+            <SystemMessage {...props} textStyle={{ color: theme.colors.palette.neutral300 }} />
+          )}
+          bottomOffset={insets.bottom}
+          renderAvatar={() => {
+            // use pravatar:
+            const groupId = "123"
+            return (
+              <Image
+                source={{ uri: `https://i.pravatar.cc/150?u=${groupId}` }}
+                style={{ width: 32, height: 32 }}
+              />
+            )
+          }}
+          maxComposerHeight={100}
+          textInputProps={themed($composer)}
+          renderBubble={(props) => {
+            return (
+              <Bubble
                 {...props}
-                containerStyle={{
-                  justifyContent: 'center',
-                }}>
-                <Ionicons name="send" color={theme.colors.palette.primary300} size={28} />
-              </Send>
-            )}
-          </View>
-        )}
-        renderInputToolbar={renderInputToolbar}
-        renderChatFooter={() => (
-          <ReplyMessageBar clearReply={() => setReplyMessage(null)} message={replyMessage} />
-        )}
-        // onLongPress={(context, message) => setReplyMessage(message)}
-        renderMessage={(props) => (
-          <ChatMessageBox
-            {...props}
-            setReplyOnSwipeOpen={setReplyMessage}
-            updateRowRef={updateRowRef}
-          />
-        )}
-      />
-    </SafeAreaView>
-    // </ImageBackground>
-  );
-};
+                textStyle={{
+                  right: {
+                    color: "#000",
+                  },
+                }}
+                wrapperStyle={{
+                  left: {
+                    backgroundColor: "#fff",
+                  },
+                  right: {
+                    backgroundColor: theme.colors.palette.secondary300,
+                  },
+                }}
+              />
+            )
+          }}
+          // renderAccessory={() => {
+          //   const groupId = "456";
+          //   return <Image source={{ uri: `https://i.pravatar.cc/150?u=${groupId}` }} style={{ width: 32, height: 32 }} />
+          // }}
+          isTyping={true}
+          infiniteScroll
+          onPressActionButton={() => {
+            console.log("action button pressed")
+          }}
+          isScrollToBottomEnabled={true}
+          renderSend={(props) => (
+            <View
+              style={{
+                height: 44,
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                gap: 14,
+                paddingHorizontal: 14,
+              }}
+            >
+              {text === "" && (
+                <>
+                  <Ionicons name="camera-outline" color={theme.colors.palette.primary300} size={28} />
+                  <Ionicons name="mic-outline" color={theme.colors.palette.primary300} size={28} />
+                </>
+              )}
+              {text !== "" && (
+                <Send
+                  {...props}
+                  containerStyle={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <Ionicons name="send" color={theme.colors.palette.primary300} size={28} />
+                </Send>
+              )}
+            </View>
+          )}
+          renderInputToolbar={renderInputToolbar}
+          renderChatFooter={() => (
+            <ReplyMessageBar clearReply={() => setReplyMessage(null)} message={replyMessage} />
+          )}
+          // onLongPress={(context, message) => setReplyMessage(message)}
+          renderMessage={(props) => (
+            <ChatMessageBox
+              {...props}
+              setReplyOnSwipeOpen={setReplyMessage}
+              updateRowRef={updateRowRef}
+            />
+          )}
+        />
+      {/* </View> */}
+    </ImageBackground>
+  )
+}
 
 const $composer: ThemedStyle<ViewStyle> = ({ colors, spacing }) => ({
-  backgroundColor: '#fff',
+  backgroundColor: "#fff",
   borderRadius: 18,
   borderWidth: 1,
   borderColor: colors.palette.neutral300,
@@ -230,9 +251,7 @@ const $composer: ThemedStyle<ViewStyle> = ({ colors, spacing }) => ({
 //   },
 // });
 
-export default Page;
-
-
+export default Page
 
 // // Page.tsx
 // const Page = () => {
@@ -289,10 +308,10 @@ export default Page;
 //           },
 //         }),
 //       }));
-      
+
 //       return GiftedChat.append(previousMessages, messagesToAdd);
 //     });
-    
+
 //     // Clear reply state after sending
 //     setReplyMessage(null);
 //   }, [replyMessage]);
