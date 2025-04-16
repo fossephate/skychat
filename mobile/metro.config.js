8fa2b7bddc2d630501f117f6c29fdab4f5e05438
Matthew Fosse <matt@fosse.co>
2025-04-16 12:21:26 -0700

    working

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,39 +1,76 @@
 /* eslint-env node */
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config")
-const path = require("path")
+const { getDefaultConfig } = require("expo/metro-config");
+const { FileStore } = require('metro-cache');
+const path = require("path");
+const fs = require('fs');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
 config.transformer.getTransformOptions = async () => ({
   transform: {
-    // Inline requires are very useful for deferring loading of large dependencies/components.
-    // For example, we use it in app.tsx to conditionally load Reactotron.
-    // However, this comes with some gotchas.
-    // Read more here: https://reactnative.dev/docs/optimizing-javascript-loading
-    // And here: https://github.com/expo/expo/issues/27279#issuecomment-1971610698
     inlineRequires: true,
   },
-})
+});
 
 // This helps support certain popular third-party libraries
 // such as Firebase that use the extension cjs.
-config.resolver.sourceExts.push("cjs")
+config.resolver.sourceExts.push("cjs");
 
 // Find the project and workspace directories
 const projectRoot = __dirname;
-// This can be replaced with `find-yarn-workspace-root`
-const monorepoRoot = path.resolve(projectRoot, '../..');
+const monorepoRoot = path.resolve(projectRoot, '..');
 
+// Watch all folders in the monorepo
 config.watchFolders = [monorepoRoot];
 
+// Add node_modules resolution paths
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(monorepoRoot, 'node_modules'),
 ];
 
-module.exports = config
+// Force resolution for problematic packages
+const extraNodeModules = {};
 
+// Specifically resolve these packages to the root node_modules
+const packagesToResolve = [
+  'react',
+  'react-dom',
+  'react-native',
+  'expo',
+  'expo-modules-core',
+  'expo-localization',
+  'fast-text-encoding',
+  'react-native-get-random-values',
+  '@ethersproject/shims',
+  '@react-navigation/native',
+  '@react-navigation/bottom-tabs',
+  '@react-navigation/elements',
+  '@react-navigation/core',
+  'react-native-reanimated',
+  'react-native-gesture-handler',
+  'react-native-safe-area-context'
+];
 
+// Add each package to extraNodeModules with explicit path
+packagesToResolve.forEach(pkg => {
+  const packagePath = path.resolve(monorepoRoot, 'node_modules', pkg);
+  if (fs.existsSync(packagePath)) {
+    extraNodeModules[pkg] = packagePath;
+  }
+});
 
+config.resolver.extraNodeModules = extraNodeModules;
+
+// Make sure symlinks work
+config.resolver.disableHierarchicalLookup = false;
+config.resolver.enableWorkspacesSymlinksResolution = true;
+
+// Use turborepo to restore the cache when possible
+config.cacheStores = [
+  new FileStore({ root: path.join(__dirname, 'node_modules', '.cache', 'metro') }),
+];
+
+module.exports = config;
